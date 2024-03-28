4b4db45a24c9298b46070e067f433303730b0a10
spider <burkstremane@gmail.com>
2024-03-28 00:46:24 -0600

    test-2

--- source.js
+++ source.js
@@ -1,5 +1,6 @@
 import {mojo} from './index.js'
-console.log('hello mojo')
+console.log('hello mojo');
+console.log('testing api')
 window.mojo = mojo
 for (const prop in mojo) {
     window[prop] = mojo[prop];
